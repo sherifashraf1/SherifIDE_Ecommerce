51751d2f4052617c70c2310e5d37409b5090ae05
sherifashraf910 <sherifashraf910@gmail.com>
2019-09-03 16:57:10 +0200

    adding uperCase func to text ,  adding TOKYO and NANCY slides

--- Fabz/Modules/Walkthrough/ViewController/WalkthroughViewController.swift
+++ Fabz/Modules/Walkthrough/ViewController/WalkthroughViewController.swift
@@ -29,11 +29,60 @@ class WalkthroughViewController: UIViewController {
             $0.transform = CGAffineTransform(scaleX: 1.4, y: 1.4)
         }
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        slides = [createSlide1(),createSlide2(),createSlide3()]
+        setupSlideScrollView(slides: slides)
+        pageIndicatorSetUp()
+        pageControl.numberOfPages = slides.count
+        pageControl.currentPage = 0
+        view.bringSubviewToFront(pageControl)
+        autoPageSlideWithTimer()
+    }
     
     fileprivate func pageIndicatorSetUp() {
         let image = UIImage.outlinedEllipse(size: CGSize(width: 7.0, height: 7.0), color: .white)
         pageControl.pageIndicatorTintColor = UIColor.init(patternImage: image!)
         pageControl.currentPageIndicatorTintColor = .white
+    }
+
+    private func createSlide1()-> Slide{
+        let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide1.slide1SubLineView.backgroundColor = UIColor.IDEColor
+        slide1.lineView.addSubview(slide1.slide1SubLineView)
+        slide1.imageView.image = UIImage(named: "slide1")
+        slide1.imageTitle.text = "Shop Everywere"
+        slide1.imageDescription.text = "High quality material egyption fashion online store".uppercased()
+        return slide1
+    }
+    
+    private func createSlide2()-> Slide{
+        let slide2:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide2.slide2SubLineView.backgroundColor = UIColor.IDEColor
+        slide2.lineView.addSubview(slide2.slide2SubLineView)
+        slide2.imageView.image = UIImage(named: "slide2")
+        slide2.imageTitle.text = "Money Heist"
+        slide2.imageDescription.text = "Je m’appelle Tokyo. Mais ce n’était pas mon nom quand toute.".uppercased()
+        return slide2
+    }
+    
+    private func createSlide3()-> Slide{
+        let slide3:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide3.slide3SubLineView.backgroundColor = UIColor.IDEColor
+        slide3.lineView.addSubview(slide3.slide3SubLineView)
+        slide3.imageView.image = UIImage(named: "slide3")
+        slide3.imageTitle.text = "Nancy Ajram"
+        slide3.imageDescription.text = "one of the most successful singers in the Arab world".uppercased()
+        return slide3
+    }
+
+    func setupSlideScrollView(slides : [Slide]) {
+        scrollView.isPagingEnabled = true
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width , height: view.frame.height)
+            scrollView.addSubview(slides[i])
+        }
+        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
     }
     
     fileprivate func autoPageSlideWithTimer() {
@@ -50,58 +99,6 @@ class WalkthroughViewController: UIViewController {
         self.i += 1
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        slides = [createSlide1(),createSlide2(),createSlide3()]
-        setupSlideScrollView(slides: slides)
-        pageIndicatorSetUp()
-        pageControl.numberOfPages = slides.count
-        pageControl.currentPage = 0
-        view.bringSubviewToFront(pageControl)
-        autoPageSlideWithTimer()
-    }
-    
-    private func createSlide1()-> Slide{
-        let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
-        slide1.slide1SubLineView.backgroundColor = UIColor.IDEColor
-        slide1.lineView.addSubview(slide1.slide1SubLineView)
-        slide1.imageView.image = UIImage(named: "slide1")
-        slide1.imageTitle.text = "Shop Everywere"
-        slide1.imageDescription.text = "HIGH QUALITY MATERIAL EGYPTION FASHION ONLINE STORE"
-        return slide1
-
-    }
-    
-    private func createSlide2()-> Slide{
-        let slide2:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
-        slide2.slide2SubLineView.backgroundColor = UIColor.IDEColor
-        slide2.lineView.addSubview(slide2.slide2SubLineView)
-        slide2.imageView.image = UIImage(named: "slide2")
-        slide2.imageTitle.text = "IDEAcademy"
-        slide2.imageDescription.text = "A GREAT COMMUNITY FOR LEARNING IOS DEVELOPMENT"
-        return slide2
-    }
-    
-    private func createSlide3()-> Slide{
-        let slide3:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
-        slide3.slide3SubLineView.backgroundColor = UIColor.IDEColor
-        slide3.lineView.addSubview(slide3.slide3SubLineView)
-        slide3.imageView.image = UIImage(named: "slide3")
-        slide3.imageTitle.text = "Sherif Ashraf Hassan"
-        slide3.imageDescription.text = "IOS DEVELOPER"
-        return slide3
-    }
-    
-    
-    func setupSlideScrollView(slides : [Slide]) {
-        scrollView.isPagingEnabled = true
-        for i in 0 ..< slides.count {
-            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width , height: view.frame.height)
-            scrollView.addSubview(slides[i])
-        }
-        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
-    }
-    
     @IBAction func skipBtn(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
@@ -115,9 +112,7 @@ extension WalkthroughViewController : UIScrollViewDelegate {
         if scrollView.contentOffset.y != 0 {
             scrollView.contentOffset.y = 0
         }
-        
     }
-    
 }
 
 
