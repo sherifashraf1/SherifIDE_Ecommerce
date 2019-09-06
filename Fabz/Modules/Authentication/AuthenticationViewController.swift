29faf032c74a104453faeeee6d8f942d14533e45
sherifashraf910 <sherifashraf910@gmail.com>
2019-09-06 07:02:12 +0200

    Finishing Authentication screen..

--- Fabz/Modules/Authentication/AuthenticationViewController.swift
+++ Fabz/Modules/Authentication/AuthenticationViewController.swift
@@ -17,40 +17,110 @@ class AuthenticationViewController: UIViewController {
     @IBOutlet weak var userNameTxtField: UITextField!
     @IBOutlet weak var emailTxtField: UITextField!
     @IBOutlet weak var passwordTxtField: UITextField!
-
+    
     @IBOutlet weak var userNameTxtFLine: UIView!
     @IBOutlet weak var emailTxtFLine: UIView!
     @IBOutlet weak var passwordTxtFLine: UIView!
     @IBOutlet weak var userNameStackView: UIStackView!
+    @IBOutlet weak var checkBoxBtn: UIButton!
+    
+    @IBOutlet weak var agreeWithTermsStackView: UIStackView!
+    
+    @IBOutlet weak var signUpBtn: CustomBTN!
+    
+    @IBOutlet weak var faceBookBtn: UIButton!
+    
+    @IBOutlet weak var sigbUpBtnButtomConstant: NSLayoutConstraint!
+    
+    @IBOutlet weak var forgetPasswordBtn: UIButton!
+    
     
     fileprivate func addLineUnderAuthStackView() {
         let underAuthStackLineView = UIView(frame: CGRect(x: 40, y: 262, width: view.frame.width, height: 0.5))
-         underAuthStackLineView.backgroundColor = UIColor(displayP3Red: 216/255, green: 216/255, blue: 216/255, alpha: 0.2)
+        underAuthStackLineView.backgroundColor = UIColor(displayP3Red: 216/255, green: 216/255, blue: 216/255, alpha: 0.2)
         view.addSubview(underAuthStackLineView)
+    }
+    
+    fileprivate func addIconsToTextFields() {
+        let imageView1 = UIImageView(frame: CGRect(x: 0, y: 8, width: 20, height: 20))
+        let userNameIcon = UIImage(named: "username-icon")
+        imageView1.image = userNameIcon
+        imageView1.contentMode = .scaleAspectFit
+        
+        
+        let imageView2 = UIImageView(frame: CGRect(x: 0, y: 8, width: 20, height: 20))
+        let emailIcon = UIImage(named: "email-icon")
+        imageView2.image = emailIcon
+        imageView2.contentMode = .scaleAspectFit
+        
+        let imageView3 = UIImageView(frame: CGRect(x: 0, y: 8, width: 20, height: 20))
+        let passwordIcon = UIImage(named: "password-icon")
+        imageView3.image = passwordIcon
+        imageView3.contentMode = .scaleAspectFit
+        
+        
+        let view1 = UIView(frame: CGRect(x: 0, y: 0, width: 35, height: 40))
+        view1.addSubview(imageView1)
+        view1.backgroundColor = .clear
+        
+        
+        let view2 = UIView(frame: CGRect(x: 0, y: 0, width: 35, height: 40))
+        view2.addSubview(imageView2)
+        view2.backgroundColor = .clear
+        
+        
+        let view3 = UIView(frame: CGRect(x: 0, y: 0, width: 35, height: 40))
+        view3.addSubview(imageView3)
+        view3.backgroundColor = .clear
+        
+        
+        userNameTxtField.leftViewMode = .always
+        userNameTxtField.leftView = view1
+        emailTxtField.leftViewMode = .always
+        emailTxtField.leftView = view2
+        passwordTxtField.leftViewMode = .always
+        passwordTxtField.leftView = view3
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         signUpButttonLine.backgroundColor = .white
         addLineUnderAuthStackView()
+        addIconsToTextFields()
+        checkBoxBtn.layer.cornerRadius = 5
+        
     }
     
- 
+    
     private func configSignupBtn(){
         self.loginButton.isHighlighted = true
         self.loginButtomLine.backgroundColor = .clear
         self.signUpButttonLine.backgroundColor = .white
         userNameStackView.isHidden = false
+        agreeWithTermsStackView.isHidden = false
+        faceBookBtn.isHidden = false
+        signUpBtn.setTitle("SIGNUP", for: .normal)
+        sigbUpBtnButtomConstant.constant -= 54
+
+        forgetPasswordBtn.isHidden = true
+
         emailTxtField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: UIColor(white: 0.6, alpha: 0.4)])
         emailTxtFLine.backgroundColor = UIColor(white: 0.6, alpha: 0.4)
-
+        
     }
     private func configLoginBtn(){
         self.signUpButton.isHighlighted = true
         self.loginButtomLine.backgroundColor = .white
         self.signUpButttonLine.backgroundColor = .clear
         userNameStackView.isHidden = true
-        emailTxtField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        agreeWithTermsStackView.isHidden = true
+       
+        faceBookBtn.isHidden = true
+        signUpBtn.setTitle("LOGIN", for: .normal)
+        sigbUpBtnButtomConstant.constant += 54
+        
+        forgetPasswordBtn.isHidden = false
+    emailTxtField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
         emailTxtFLine.backgroundColor = .white
         
         
@@ -62,6 +132,16 @@ class AuthenticationViewController: UIViewController {
     @IBAction func loginBtn(_ sender: Any) {
         configLoginBtn()
     }
+    
+    
+    @IBAction func checkBoxBtn(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "checked"), for: .normal)
+        sender.tintColor = .white
+        sender.isSelected = !sender.isSelected
+        if !sender.isSelected {
+            sender.setImage(UIImage(named: "unchecked"), for: .normal)
 
+        }
+    }
     
 }
